f6ad9aec3163bd9779f1e04426d4e5bdd1f8b47e
Douglas Lassance <douglassance@gmail.com>
2015-01-19 12:26:05 -0800

    Added custom code

---
+++ Source/Kick/StateMachineComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "StateMachineComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(EditInlineNew, meta = (BlueprintSpawnableComponent))
+class KICK_API UStateMachineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
